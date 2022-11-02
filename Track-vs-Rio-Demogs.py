bb84398eacb1391a4a504df79afbd1ef9d4bcc6f
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2022-11-02 09:55:05 +0000

    Update Track-vs-Rio-Demogs.py

--- Track-vs-Rio-Demogs.py
+++ Track-vs-Rio-Demogs.py
@@ -5,15 +5,17 @@ from operator import itemgetter
 import itertools
 import re
 from ordered_set import OrderedSet
-
 os.chdir('M:\Rio vs Trak')
 
+# define datasets
 rio_full = pd.read_csv('RIO_Full.csv', parse_dates=True)
 trak_full = pd.read_csv('Trak_Full.csv', parse_dates=True)
 
-#replace NaNs with empty string to enable address joins
+# replace NaNs with empty string to enable address joins and ensure no duplicates
 rio_full.fillna('', inplace=True)
+rio_full.drop_duplicates(inplace=True)
 trak_full.fillna('', inplace=True)
+trak_full.drop_duplicates(inplace=True)
 
 # build address join column in each dataframe and remove blank values
 rio_full['AddressJoin'] = ((rio_full['Address1'].str.lower() + ', ' +  
@@ -21,56 +23,63 @@ rio_full['AddressJoin'] = ((rio_full['Address1'].str.lower() + ', ' +
                                rio_full['Address3'].str.lower() + ', ' + 
                                rio_full['Address4'].str.lower() + ', ' + 
                                rio_full['PostCode'].str.lower()
+                            # remove unwanted text
                             .str.replace('zz99', ''))
                             .str.replace('channel islands', '', regex=True)
                             .str.replace('guernsey', '', regex=True)
                             .str.strip(',').str.strip()
                           )
+# remove empty cells
 rio_full['AddressJoin'] = rio_full['AddressJoin'].str.replace(' ,', '')
 
 
 trak_full['AddressJoin'] = ((trak_full['Address1'].str.lower() + ', ' +  
                                trak_full['Address2'].str.lower() + ', ' + 
                                trak_full['PostCode'].str.lower())
+                            # remove unwanted text
                             .str.replace('channel islands', '', regex=True)
                             .str.replace('guernsey', '', regex=True)
                             .str.strip(',').str.strip()
                            )
+# remove empty cells
 trak_full['AddressJoin'] = rio_full['AddressJoin'].str.replace(' ,', '')
 
-# merge into one df
+# merge into one dataframe
 merged_dfs = pd.concat([trak_full, rio_full], ignore_index=True, axis=0)
 
-# remobve unvanyed address columns
+# remove unwanted address columns that were joined previously
 merged_dfs.drop(['Address1', 'Address2', 'PostCode','Address3','Address4' ], axis=1, inplace=True)
 
-# seperate records that appear in both trak and rio with those that don't
+# separate records that appear in both Trak and Rio with those that don't
 urns_in_trak_and_rio = merged_dfs[merged_dfs['URN'].map(merged_dfs['URN'].value_counts()) > 1]
 not_urns_in_trak_and_rio = merged_dfs[merged_dfs['URN'].map(merged_dfs['URN'].value_counts()) == 1]
 
 master_list = []
 
+# group rows by URN  and convert to a nestled list
 for group_name, group in urns_in_trak_and_rio.groupby('URN'):
     group_list = group.values.tolist() 
     master_list.append(group_list)   
     
-# define new columns
-cols = urns_in_trak_and_rio.columns.tolist()
-cols.append('Ratio')
+    # # define new columns
+# cols = urns_in_trak_and_rio.columns.tolist()
+# cols.append('Ratio')
 
-# create new DataFrame to hold results
+# create new dataframe to hold results
 df_results = pd.DataFrame(columns = cols)
 
+# separate the string to perform fuzzy matching on and create nestled list
 address_pairs = [] 
 for i in range(len(master_list)):
     trak_address = master_list[i][0][6]
     rio_address = master_list[i][1][6]
     address_pairs.append([trak_address, rio_address])
-
-fuzz_score = []
+    
+    fuzz_score = []
 diffA_list = []
 diffB_list = []
 
+# perform fuzzy match normalising case and append result to list
 for i in range(len(address_pairs)):
     x = address_pairs[i][0].lower()
     y = address_pairs[i][1].lower()
@@ -79,6 +88,7 @@ for i in range(len(address_pairs)):
     master_list[i][0].append(ratio)
     master_list[i][1].append(ratio)
     
+    # split the address strings and get differences between strings
     splitA = OrderedSet(x.split(' '))
     splitB = OrderedSet(y.split(' '))
     diffA = splitA - splitB
@@ -91,9 +101,10 @@ for i in range(len(address_pairs)):
         diffB = ' '.join(diffB) 
         master_list[i][1].append(diffB)
         
-master_list
+# flatten nestled list to enable conversion into dataframe
 flattened_master_list = [i for s in master_list for i in s]
 
+# define columns for results dataframe (currently flattened_master_list)
 cols = ['System', 
         'URN', 
         'FirstName', 
@@ -106,7 +117,10 @@ cols = ['System',
         'Score', 
         'Diffs']
 
+# convert flattened_master_list into dataframe
 df_res = pd.DataFrame(flattened_master_list, columns = cols)
+
+# re-order columns
 df_res = df_res[['System', 
         'URN', 
         'RIO_ID',
@@ -119,16 +133,18 @@ df_res = df_res[['System',
         'Diffs',
         'RIO_AddressUpdateDate']]
 
+# sort results dataframe
 df_res.sort_values(by=['Score', 'URN'], ascending=(False, True), inplace=True)
 
+# export
 df_res.to_excel('ratio_test.xlsx', index=False)
 
-# number of Track rows
+# reporting
+# number of Trak rows
 num_trak_rows = len(df_res[df_res['System'] == 'TRAK'])
 print(f"Number of Track Rows: {num_trak_rows}")
 
-# number of RIO rows
+# number of Rio rows
 num_rio_rows = len(df_res[df_res['System'] == 'RIO'])
 print(f"Number of RIO Rows:   {num_rio_rows}")
-
 print(f'Difference: {num_rio_rows - num_trak_rows }')
